471a739d8d549608b5e343421a56abf4817b9cd7
noprom <tyee.noprom@qq.com>
2015-06-30 16:37:46 +0800

    cal done

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+- (IBAction)compute;
+@property (weak, nonatomic) IBOutlet UITextField *txtNum1;
+@property (weak, nonatomic) IBOutlet UITextField *txtNum2;
+@property (weak, nonatomic) IBOutlet UILabel *lableResult;
+
 @end
 
 @implementation ViewController
@@ -24,4 +29,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+// 点击计算
+- (IBAction)compute {
+    
+    // 1.获取用户的输入
+    // 2.计算求和
+    // 3.显示结果
+    NSString *num1 = self.txtNum1.text;
+    NSString *num2 = self.txtNum2.text;
+    
+    int n1 = [num1 intValue];
+    int n2 = [num2 intValue];
+    int result = n1 + n2;
+    self.lableResult.text = [NSString stringWithFormat:@"%d",result];
+    NSLog(@"点击了计算");
+}
 @end
